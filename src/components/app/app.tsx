1547482e612635d40c8423df456aa08e4e05d71a
Надежда <shutko_bstu@mail.ru>
2024-07-16 21:23:06 +0300

    Исправит ошибки форматирования

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,13 @@
-import MainPage from "../../pages/main/main-page";
+import MainPage from '../../pages/main/main-page';
 
 type AppScreenProps = {
     placesCount: number;
   }
-  
+
 function App({placesCount}: AppScreenProps): JSX.Element {
-    return (
-        <MainPage placesCount={placesCount}/>
-    )
-};
+  return (
+    <MainPage placesCount={placesCount}/>
+  );
+}
 
 export default App;
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
 export const Setting = {
-    PlacesCount: 312
-  };
+  PlacesCount: 312
+};
