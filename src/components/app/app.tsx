98b06cc990e026dbf3b5169e33aacbc2ffcc25dc
Надежда <shutko_bstu@mail.ru>
2024-07-16 21:17:51 +0300

    Создаст компоненты основных страниц, получит данные из пропсов для  главной страницы

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainPage from "../../pages/main/main-page";
+
+type AppScreenProps = {
+    placesCount: number;
+  }
+  
+function App({placesCount}: AppScreenProps): JSX.Element {
+    return (
+        <MainPage placesCount={placesCount}/>
+    )
+};
+
+export default App;
