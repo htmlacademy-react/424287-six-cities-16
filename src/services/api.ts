82c80c74e6b41275bc5c8da39394f9cc84fb4546
Надежда <shutko_bstu@mail.ru>
2024-08-19 17:11:42 +0300

    Отобразит сообщение об ошибке

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,22 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosError, AxiosResponse} from 'axios';
+import { getToken } from './token';
+import {toast} from 'react-toastify';
+import { StatusCodes } from 'http-status-codes';
+
+type DetailMessageType = {
+    type: string;
+    message: string;
+  }
 
 const BACKEND_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
@@ -9,5 +24,27 @@ export const createAPI = (): AxiosInstance => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  api.interceptors.request.use(
+    (config:AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<DetailMessageType>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        const detailMessage = (error.response.data);
+        toast.warn(detailMessage.message);
+      }
+
+      throw error;
+    }
+  );
   return api;
 };
